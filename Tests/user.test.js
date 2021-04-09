bd63452a4a3d87b13993f338b073715efa86386e
cody <codyvela0702@gmail.com>
2021-04-09 15:56:40 -0500

    update tests

--- Tests/user.test.js
+++ Tests/user.test.js
@@ -1,67 +1,65 @@
-const userModel = require('../Models/User');
+const userModel = require("../Models/User");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 describe("User", () => {
-    beforeAll(async () => {
-        try {
-          //Creating a new mongoose promise for database
-          let database = (mongoose.Promise = global.Promise);
-          await mongoose.connect(process.env.MONGO_CONNECT_KEY, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-          });
+  beforeAll(async () => {
+    try {
+      //Creating a new mongoose promise for database
+      let database = (mongoose.Promise = global.Promise);
+      await mongoose.connect(process.env.MONGO_CONNECT_KEY, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      });
 
-          //Check if the connection was successful and display message.
-          if (database) {
-            console.log("Database Status: Connected");
-          }
-        } catch (error) {
-          if (error) {
-            console.log(error);
-          }
-        }
-    })
+      //Check if the connection was successful and display message.
+      if (database) {
+        console.log("Database Status: Connected");
+      }
+    } catch (error) {
+      if (error) {
+        console.log(error);
+      }
+    }
+  });
 
-    it("Insert one user into the database", async () => {
-      const newUserInfo = {
-        firstName: "Cody",
-        lastName: "Vela",
-        userName: "cvela09",
-        email: "cvel@test.com",
-        password: "testpassword",
-        isInstructor: false
-      };
-
+  it("Insert one user into the database", async () => {
+    const newUserInfo = {
+      firstName: "Cody",
+      lastName: "Vela",
+      userName: "cvela09",
+      email: "cvel@test.com",
+      password: "testpassword",
+      isInstructor: false,
+    };
+    try {
       newUserInfo.password = await encryptPassword(newUserInfo.password);
 
       const createdUser = new userModel(newUserInfo);
 
       expect(createdUser._id).toBeDefined;
       expect(createdUser.firstName).toBe(newUserInfo.firstName);
-    });
-    it("Return All Users", async () => {
-      let listOfUsers = await userModel.find();
-      expect(listOfUsers).toBeDefined();
+    } catch (error) {
+      console.log("error in insert one");
+    }
+  });
+  it("Return All Users", async () => {
+    let listOfUsers = await userModel.find();
+    expect(listOfUsers).toBeDefined();
+  });
+  it("Find user by ID", async () => {
+    let id = "605b90cefc71acf3f126fdb9";
+    let foundUser = await userModel.findOne({ _id: id });
 
-
-    });
-    it("Find user by ID", async() => {
-      let id = "605b90cefc71acf3f126fdb9";
-      let foundUser = await userModel.findOne({_id: id})
-
-      expect(foundUser).toBeDefined()
-    })
-    afterAll(async () => {
-        userModel.deleteMany()
-        await mongoose.connection.close(); 
-    })
-
-    
-})
-
+    expect(foundUser).toBeDefined();
+  });
+  afterAll(async () => {
+    userModel.deleteMany();
+    await mongoose.connection.close();
+  });
+});
 
 //Helper Functions
 const encryptPassword = async (password) => {
