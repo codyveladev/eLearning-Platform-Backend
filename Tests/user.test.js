73fd37d401bb893d1298284ef4dc974e42da9188
cody <codyvela0702@gmail.com>
2021-04-13 22:57:18 -0500

    Update user.test.js

--- Tests/user.test.js
+++ Tests/user.test.js
@@ -1,7 +1,10 @@
 import userModel from "../models/User.js";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+<<<<<<< Updated upstream
 dotenv.config();
+=======
+>>>>>>> Stashed changes
 
 describe("User", () => {
   beforeAll(async () => {
