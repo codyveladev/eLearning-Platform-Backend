5f50d93a83c6f86a1cb8b7a7c0998174a31bb074
cody <codyvela0702@gmail.com>
2021-04-15 08:21:42 -0500

    added controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,113 @@
 const User = require("../models/User");
 
+/**
+ * @desc  Returns all users in the database
+ * @route POST /users/all
+ * @access Public
+ */
+const getAllUsers = async (req, res) => {
+  try {
+    let users = await User.find();
+    res.send(users);
+  } catch (e) {
+    if (e) {
+      console.log(e);
+    }
+  }
+};
+module.exports.getAllUsers = getAllUsers;
 
+/**
+ * @desc  Register new user
+ * @route POST /users/register
+ * @access Public
+ */
+const registerUser = async (req, res) => {
+  //Get the user info from the request body
+  const newUserInfo = req.body;
+  newUserInfo.isInstructor = false;
+
+  //Check if the username already exists
+  let foundByUserName = await User.findOne({
+    userName: newUserInfo.userName,
+  });
+  //Handler error
+  if (foundByUserName) {
+    res.status(409).send("Username already exists...");
+    console.log("Username already exists...");
+    return;
+  }
+
+  //check if the email already exists
+  let foundByEmail = await User.findOne({ email: newUserInfo.email });
+  //Handle error
+  if (foundByEmail) {
+    res.status(409).send("Email already in use...");
+    console.log("Email already in use...");
+    return;
+  }
+  //Register user to database
+  try {
+    const userToStore = await User.create(newUserInfo);
+    userToStore.save();
+    if (userToStore) {
+      res.send(`User with ID ${userToStore._id} has been created!`);
+    }
+  } catch (error) {
+    res.send(error);
+    console.log(error);
+  }
+};
+module.exports.registerUser = registerUser;
+
+/**
+ * @desc  Login for user
+ * @route POST /users/login
+ * @access Public
+ */
+const loginUser = async (req, res) => {
+  let userInfo = req.body;
+
+  try {
+    const foundUser = await User.findOne({ userName: userInfo.userName });
+
+    if (!foundUser) {
+      res.status(404).send("User not found!");
+      return;
+    }
+
+    let doesMatch = await foundUser.matchPassword(userInfo.password);
+
+    if (!doesMatch) {
+      res.send("Password Does Not Match...");
+      return;
+    }
+
+    res.send("Login Success!");
+  } catch (e) {
+    if (e) res.send(e);
+  }
+};
+
+module.exports.loginUser = loginUser;
+
+const getUserById = async (req, res) => {
+  //Get the user id from request
+  let userId = req.params.id;
+
+  try {
+    //Find the user in the database given the ID
+    let foundUser = await User.findById(userId).select("-password");
+
+    if (!foundUser) {
+      res.status(404).send("User not found");
+    }
+
+    //Send the information
+    res.send(foundUser);
+  } catch (e) {
+    if (e) res.status(500).send(e);
+  }
+};
+
+module.exports.getUserById = getUserById;
--- routes/users.js
+++ routes/users.js
@@ -1,129 +1,22 @@
 const express = require("express");
 const users = express.Router();
-const bcrypt = require("bcrypt");
-//Models
-const userModels = require("../models/User");
 
-/**
- * @type GET
- * @route /users/all
- * @desc returns all of the users in the database
- */
-users.get("/all", async (req, res) => {
-  try {
-    let users = await userModels.find();
-    res.send(users);
-  } catch (e) {
-    if (e) {
-      console.log(e);
-    }
-  }
-});
+//Functions from Controller
+const registerUser = require("../controllers/userControllers").registerUser;
+const loginUser = require("../controllers/userControllers").loginUser;
+const getAllUsers = require("../controllers/userControllers").getAllUsers;
+const getUserById = require("../controllers/userControllers").getUserById;
 
-/**
- * @type GET
- * @route /users/login
- * @desc login a user
- */
-users.get("/login", async (req, res) => {
-  let userInfo = req.body;
+//Register User
+users.route("/register").post(registerUser);
 
-  try {
-    const foundUser = await userModels.findOne({ userName: userInfo.userName });
+//Login User
+users.route("/login").post(loginUser);
 
-    if (!foundUser) {
-      res.send("User not found!");
-    }
+//Get All Users
+users.route("/all").get(getAllUsers);
 
-    let doesMatch = await bcrypt.compare(userInfo.password, foundUser.password);
-
-    if (!doesMatch) {
-      res.send("Password Does Not Match...");
-    }
-
-    res.send("Login Success!");
-  } catch (e) {
-    if (e) res.send(e);
-  }
-});
-
-/**
- * @type POST
- * @route /users/register
- * @desc Registers one user into the database
- */
-users.post("/register", async (req, res) => {
-  //Get the user info from the request body
-  const newUserInfo = req.body;
-  newUserInfo.isInstructor = false;
-
-  //Check if the username already exists
-  let foundByUserName = await userModels.findOne({
-    userName: newUserInfo.userName,
-  });
-  if (foundByUserName) {
-    res.status(409).send("Username already exists...");
-    console.log("Username already exists...");
-    return;
-  }
-
-  //check if the email already exists
-  let foundByEmail = await userModels.findOne({ email: newUserInfo.email });
-  if (foundByEmail) {
-    res.status(409).send("Email already in use...");
-    console.log("Email already in use...");
-    return;
-  }
-
-  //Encrypt password before storing
-  newUserInfo.password = await encryptPassword(newUserInfo.password);
-
-  try {
-    const userToStore = await userModels.create(newUserInfo);
-    userToStore.save();
-    if (userToStore) {
-      res.send(`User with ID ${userToStore._id} has been created!`);
-    }
-  } catch (error) {
-    res.send(error);
-    console.log(error);
-  }
-});
-
-/**
- * @type GET
- * @route /users/user/:id
- * @desc Finds a user by ID in the database and returns the information.
- */
-users.get("/user/:id", async (req, res) => {
-  //Get the user id from request
-  let userId = req.params.id;
-
-  try {
-    //Find the user in the database given the ID
-    let foundUser = await userModels.findOne({ _id: userId });
-
-    //Only send 3 fields omitting email, password and ID
-    let userInformationToSend = {
-      firstName: foundUser.firstName,
-      lastName: foundUser.lastName,
-      userName: foundUser.userName,
-      courses: foundUser.courses,
-    };
-
-    //Send the information
-    res.send(userInformationToSend);
-  } catch (e) {
-    if (e) res.status(404).send("User not found...");
-  }
-});
-
-//Helper Functions
-const encryptPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  return hashedPassword;
-};
+//Get User by Id
+users.route("/user/:id").get(getUserById);
 
 module.exports = users;
